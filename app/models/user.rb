918a3a93d2c96f29e5aa61e2d0f7e56843990b6c
Josef Berrios <sellout@Josefs-MacBook-Air.local>
2022-08-15 13:53:34 -0400

    made some migrations, not done yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :teams_users
+  has_many :teams, through: :teams_users
+  validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
